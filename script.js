2a8646009faeebd9d6dc101b6562f4778cce419d
sophiahyung <152532242+sophiahyung@users.noreply.github.com>
2023-12-01 10:55:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-let songRand = []
+let songRand = ["Lovers Rock By TV Girl", "Dancing Queen By ABBA", "Real Love Baby By Father John Misty", "Thinking Bout You By Frank Ocean", "Baby I love You By The Ramones","", "", "", "", ""]
