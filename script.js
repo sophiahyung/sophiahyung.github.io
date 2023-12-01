995cedde5789c72c5b9121ba39e8c005824ab299
sophiahyung <152532242+sophiahyung@users.noreply.github.com>
2023-12-01 11:02:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 let songRand = ["Lovers Rock By TV Girl", "Dancing Queen By ABBA", "Real Love Baby By Father John Misty", "Thinking Bout You By Frank Ocean", "Baby I love You By The Ramones","We Fell In Love in October By Girl in Red", "Dark Paradise by Lana Del Rey", "Don't Let Me Go by Cigarettes After Sex", "In My Mind by Lyn Lapid", "Heavenly by Cigarettes After Sex"]
+function Randomizer() {
+    console.log(arr[(Math.floor(Math.random() * arr.length))]);
+}
+Randomizer()
