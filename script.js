9e25101182e1187d5430cfcf9c0ba1de6a7a5355
sophiahyung <152532242+sophiahyung@users.noreply.github.com>
2023-12-04 11:11:37 -0800

    randomization to UI

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let songRand = ["Lovers Rock By TV Girl", "Dancing Queen By ABBA", "Real Love Baby By Father John Misty", "Thinking Bout You By Frank Ocean", "Baby I love You By The Ramones","we fell in love in october By girl in red", "Dark Paradise by Lana Del Rey", "Don't Let Me Go by Cigarettes After Sex", "In My Mind by Lyn Lapid", "Heavenly by Cigarettes After Sex"]
 function Randomizer() {
-    return(songRand[(Math.floor(Math.random() * songRand.length))]);
+    const selectedItem = (songRand[(Math.floor(Math.random() * songRand.length))]);
+    console.log(`selected song: ${selectedItem}`)
+    document.getElementById(`randomizedNodeID`).innerHTML = selectedItem
 }
