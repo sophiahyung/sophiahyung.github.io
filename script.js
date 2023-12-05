aaf837f872f93bd6d3e7ae0310b2e8dfbfc77fb8
sophiahyung <152532242+sophiahyung@users.noreply.github.com>
2023-12-05 10:57:54 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let songRand = ["Lovers Rock By TV Girl", "Dancing Queen By ABBA", "Real Love Baby By Father John Misty", "Thinking Bout You By Frank Ocean", "Baby I love You By The Ramones","we fell in love in october By girl in red", "Dark Paradise by Lana Del Rey", "Don't Let Me Go by Cigarettes After Sex", "In My Mind by Lyn Lapid", "Heavenly by Cigarettes After Sex"]
+let songRand = ["Lovers Rock By TV Girl", " Dancing Queen By ABBA", " Real Love Baby By Father John Misty", " Thinking Bout You By Frank Ocean", " Baby I love You By The Ramones"," We fell in love in october By girl in red", " Dark Paradise by Lana Del Rey", " Don't Let Me Go by Cigarettes After Sex", " In My Mind by Lyn Lapid", " Heavenly by Cigarettes After Sex"]
 function Randomizer() {
     const selectedItem = (songRand[(Math.floor(Math.random() * songRand.length))]);
     console.log(`selected song: ${selectedItem}`)
