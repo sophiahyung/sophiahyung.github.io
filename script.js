0f794d39c84d66022b141611053508aa29e973be
nerdgirl_art <creepcomix.dev@gmail.com>
2023-12-06 11:31:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,5 @@ function Randomizer() {
 let person = prompt("Please enter your name");
 
 if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
+    document.getElementById("welcomeName").innerHTML = person;
 }
