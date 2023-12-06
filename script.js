4c9ea984d87e91623a6663fdbe3d5f3f926e31c8
nerdgirl_art <creepcomix.dev@gmail.com>
2023-12-05 16:51:00 -0800

    Update script.js
    Added comments to the JS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//array of songs that can be picked in the randomizer
 let songRand = ["Lovers Rock By TV Girl", " Dancing Queen By ABBA", " Real Love Baby By Father John Misty", " Thinking Bout You By Frank Ocean", " Baby I love You By The Ramones"," We fell in love in october By girl in red", " Dark Paradise by Lana Del Rey", " Don't Let Me Go by Cigarettes After Sex", " In My Mind by Lyn Lapid", " Heavenly by Cigarettes After Sex"]
+//randomizer function
 function Randomizer() {
     const selectedItem = (songRand[(Math.floor(Math.random() * songRand.length))]);
     console.log(`selected song: ${selectedItem}`)
